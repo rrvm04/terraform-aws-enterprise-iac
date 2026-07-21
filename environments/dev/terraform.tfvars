800cfb7da48818bc08518859ecdb1ebc24fc247a
rafikahmmed-admin <rafik.ahmmed@gmail.com>
2026-07-21 15:23:40 +0530

    Add VPC module

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,4 +1,4 @@
-aws_region = "ap-south-1"
+aws_region   = "ap-south-1"
 project_name = "enterprise"
-environment = "dev"
-vpc_cidr = "10.0.0.0/16"
+environment  = "dev"
+vpc_cidr     = "10.0.0.0/16"
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -2,14 +2,14 @@ variable "aws_region" {
   type = string
 }
 variable "project_name" {
-    description = "Project Name"
-    type = string
+  description = "Project Name"
+  type        = string
 }
-variable "environment" {  
-    description = "Environment Name"
-    type = string
+variable "environment" {
+  description = "Environment Name"
+  type        = string
 }
-variable "vpc_cidr" {  
-    description = "VPC CIDR"
-    type = string
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
 }
