c44daf45a15be881b75bf3bec618d703cd77d6ed
rafikahmmed-admin <rafik.ahmmed@gmail.com>
2026-07-22 12:33:28 +0530

    Modified root config files

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -13,3 +13,26 @@ variable "vpc_cidr" {
   description = "VPC CIDR"
   type        = string
 }
+variable "public_subnet_1_cidr" {
+  type = string
+}
+
+variable "public_subnet_2_cidr" {
+  type = string
+}
+
+variable "private_subnet_1_cidr" {
+  type = string
+}
+
+variable "private_subnet_2_cidr" {
+  type = string
+}
+
+variable "availability_zone_1" {
+  type = string
+}
+
+variable "availability_zone_2" {
+  type = string
+}
